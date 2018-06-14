22b690f5bd516efc4b4dd63eac6d6b80185af53a
Peter Shatara <pshatara@gmail.com>
2018-06-13 21:35:58 -0700

    Adds Class 6 Exercise

---
+++ classExercise06.py
@@ -0,0 +1,34 @@
+import pylab as plb
+
+A = {
+    'a': None,
+    'b': None,
+    'c': None,
+    'd': None,
+    'e': None
+}
+
+def assign_values(dict):
+    for key in dict:
+        A[key] = plb.randint(0, 11)
+    return dict
+
+B = assign_values(A)
+
+def reassign_values(dict):
+    for key in dict:
+        if dict[key] < 5:
+            dict[key] = plb.normal(2, 3, 256)
+            plb.hist(dict[key], density=True, bins=24)
+            plb.pause(1)
+            plb.close()
+    return dict
+
+C = reassign_values(B)
+
+C['a'] = C.pop('b')
+
+C['c'] = C['d']
+del C['d']
+
+print('A == B == C: ', A == B == C)
